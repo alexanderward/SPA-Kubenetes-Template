e9b115c3f514e77f84314e2d57412da1374d91b3
Alexander Ward <alexander.ward1@gmail.com>
2021-04-01 10:25:18 -0500

    Django base setup with sqlite redis

--- backend/service/local/entrypoint.local.sh
+++ backend/service/local/entrypoint.local.sh
@@ -2,13 +2,15 @@ echo "[ + ] Setting up SSH for remote Interpreter use"
 /usr/sbin/sshd
 export -p > local/remote_interpreter/env_var.sh
 
-
 echo "[ + ] Copying Dependencies for IDE use"
 rm local/venv.7z
 cp /tmp/venv.7z local/venv.7z
 
-echo "[ + ] Collect Static"
+echo "[ + ] Collect Static Files for Admin Panel"
 python manage.py collectstatic --noinput
+
+echo "[ + ] Migrating Database"
+python manage.py migrate --noinput
 
 echo "[ + ] Starting server"
 python manage.py runserver 0.0.0.0:8000
---
+++ backend/service/service/settings/components/base.py
@@ -0,0 +1,113 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+APP_DIR = os.path.dirname(os.path.dirname(BASE_DIR))
+SECRET_KEY = os.environ['SECRET_KEY']
+DEBUG = eval(os.environ['DEBUG'])
+ALLOWED_HOSTS = eval(os.environ['ALLOWED_HOSTS'])
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'service.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'service.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(APP_DIR, 'local', 'db.sqlite3'),
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(APP_DIR, 'static')
+
+# REDIS
+REDIS_URL = "redis://{host}:{port}/0".format(
+    host=os.getenv('REDIS_HOST', 'localhost'),
+    port=os.getenv('REDIS_PORT', '6379')
+)
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": REDIS_URL,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        }
+    }
+}
+
+# CELERY
+BROKER_URL = REDIS_URL
+CELERY_BROKER_URL = REDIS_URL
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
